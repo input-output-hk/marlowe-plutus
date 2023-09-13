4f2f0993dcc3787ee301f0e449874daa2415e2e7
Jamie Bertram <jamie.bertram@tweag.io>
2023-09-13 17:16:51 -0400

    Clean up ci.nix

--- nix/ci.nix
+++ nix/ci.nix
@@ -1,7 +1,7 @@
 # This file is part of the IOGX template and is documented at the link below:
 # https://www.github.com/input-output-hk/iogx#39-nixcinix
 
-{ iogx, nix, inputs, inputs', pkgs, system, l, ... }:
+{ inputs, inputs', pkgs, system, ... }:
 
 {
   # includeDefaultOutputs = true;
