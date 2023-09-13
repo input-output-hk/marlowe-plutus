caaf0fc39b3573fd8b4e6c227051197ce50e8d87
Jamie Bertram <jamie.bertram@tweag.io>
2023-09-13 11:58:05 -0400

    Add validators flake output

--- nix/per-system-outputs.nix
+++ nix/per-system-outputs.nix
@@ -1,14 +1,15 @@
 # This file is part of the IOGX template and is documented at the link below:
 # https://www.github.com/input-output-hk/iogx#35-nixper-system-outputsnix
 
-{ inputs, inputs', pkgs, system, lib, projects ? null, ... }:
+{ inputs, inputs', pkgs, system, lib, haskellProjects, ... }:
 
 {
-  # packages = { };
-  # checks = { };
-  # apps = { };
-  # operables = { };
-  # oci-images = { };
-  # nomadTasks = { };
-  # foobar = { };
+  packages = {
+    validators = pkgs.runCommand "validators" { } ''
+      mkdir -p $out
+      cd $out
+      ${haskellProjects.default.hsPkgs.marlowe-plutus.components.exes.marlowe-validators}/bin/marlowe-validators
+      rm *.tsv
+    '';
+  };
 }
