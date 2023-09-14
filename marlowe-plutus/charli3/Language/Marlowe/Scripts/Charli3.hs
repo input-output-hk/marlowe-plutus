fcb1476f0947cc8d1f582fb4c73e7577be1bd156
Brian W Bush <brian.bush@iohk.io>
2023-09-14 16:13:44 -0600

    PLT-7535 Documented trace output for Charli3 script.

--- marlowe-plutus/charli3/Language/Marlowe/Scripts/Charli3.hs
+++ marlowe-plutus/charli3/Language/Marlowe/Scripts/Charli3.hs
@@ -113,43 +113,38 @@ mkValidator
             Spending txOutRef ->
               case find (\TxInInfo{txInInfoOutRef} -> txInInfoOutRef == txOutRef) txInfoInputs of
                 Just input -> input
-                Nothing -> traceError "C3a"
-            _ -> traceError "C3b"
+                Nothing -> traceError "C3a" -- Impossible, script input not found.
+            _ -> traceError "C3b" -- Not a spending script.
         TxInInfo{txInInfoResolved = TxOut{txOutAddress = ownAddress, txOutValue = ownValue, txOutDatum = ownDatum}} = ownInput
 
-        ownDatumOk =
+        ownOutputOk =
           case filter (\TxOut{txOutAddress} -> ownAddress == txOutAddress) txInfoOutputs of
             [TxOut{txOutValue, txOutDatum}] -> txOutValue == ownValue && txOutDatum == ownDatum
-            _ -> traceError "C3c"
-
-        (charli3Value, PV2.Datum charli3Datum) =
-          case txInfoReferenceInputs of
-            [TxInInfo{txInInfoResolved = TxOut{txOutValue, txOutDatum = OutputDatum datum}}, _] -> (txOutValue, datum)
-            [_, TxInInfo{txInInfoResolved = TxOut{txOutValue, txOutDatum = OutputDatum datum}}] -> (txOutValue, datum)
-            _ -> traceError "C3d"
+            _ -> traceError "C3c" -- Script value or datum changed.
+        PV2.Datum charli3Datum =
+          case filter
+            (\TxInInfo{txInInfoResolved = TxOut{txOutValue}} -> valueOf txOutValue charli3CurrencySymbol charli3TokenName > 1)
+            txInfoReferenceInputs of
+            [TxInInfo{txInInfoResolved = TxOut{txOutDatum = OutputDatum datum}}] -> datum
+            _ -> traceError "C3d" -- Charli3 oracle feed not found.
         charli3Oracle =
           case C3.getPriceDatas . C3.OracleFeed $ charli3Datum of
             [pd] -> C3.getPrice . C3.getPriceMap $ pd
-            _ -> traceError "C3e"
-        charli3ValueOk = valueOf charli3Value charli3CurrencySymbol charli3TokenName > 1
-
+            _ -> traceError "C3e" -- Precisely one Charli3 price value not found.
         (currencySymbol, roleName) = do
           let valuesList = AssocMap.toList $ getValue ownValue
           case valuesList of
             [(possibleAdaSymbol, _), (currencySymbol, AssocMap.toList -> [(roleName, _)])]
               | possibleAdaSymbol == adaSymbol -> (currencySymbol, roleName)
             [(currencySymbol, AssocMap.toList -> [(roleName, _)]), _] -> (currencySymbol, roleName)
-            _ -> traceError "C3f"
-
+            _ -> traceError "C3f" -- Precisely one role token not found.
         marloweValidatorAddress = scriptHashAddress marloweValidatorHash
         marloweInput = case find (\TxInInfo{txInInfoResolved} -> txOutAddress txInInfoResolved == marloweValidatorAddress) txInfoInputs of
           Just input -> input
-          Nothing -> traceError "C3g"
-
+          Nothing -> traceError "C3g" -- Marlowe script UTXO not found.
         threadTokenOk = do
           let marloweValue = txOutValue $ txInInfoResolved marloweInput
-          traceIfFalse "C3h" (valueOf marloweValue currencySymbol threadTokenName > 0)
-
+          traceIfFalse "C3h" (valueOf marloweValue currencySymbol threadTokenName > 0) -- Marlowe thread token not found.
         marloweRedeemerOk = do
           let TxInInfo{txInInfoOutRef = marloweTxOutRef} = marloweInput
               inputContentUsesRole (V1.IChoice (V1.ChoiceId choiceId (V1.Role role)) choiceNum) = choiceId == charli3ChoiceName && role == roleName && choiceNum == charli3Oracle
@@ -158,16 +153,17 @@ mkValidator
               inputUsesRole (V1.Scripts.Input inputContent) = inputContentUsesRole inputContent
               inputs :: V1.Scripts.MarloweInput
               inputs = case AssocMap.lookup (Spending marloweTxOutRef) txInfoRedeemers of
-                Nothing -> traceError "C3i"
+                Nothing -> traceError "C3i" -- Marlowe redeemer not found.
                 Just (Redeemer bytes) -> case fromBuiltinData bytes of
                   Just inputs -> inputs
-                  Nothing -> traceError "C3j"
+                  Nothing -> traceError "C3j" -- Marlowe redeemer not decoded.
           isJust $ find inputUsesRole inputs
 
-    ownDatumOk && charli3ValueOk && threadTokenOk && marloweRedeemerOk
+    ownOutputOk && threadTokenOk && marloweRedeemerOk
 
 validator :: CompiledCode (BuiltinData -> BuiltinData -> BuiltinData -> ())
 validator = do
+  -- FIXME: Hard-coded values used for proof-of-concept demonstration only.
   let charli3CurrencySymbol = "adb6f28429c860672e877f15f505a39899c1cdef9c68b2081a172736"
       charli3TokenName = "OracleFeed"
       charli3ChoiceName = "Charli3 ADAUSD"
