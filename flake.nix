4c290dada689f866df2823de60df234bdaaf1246
zeme-wana <15709674+zeme-wana@users.noreply.github.com>
2023-10-23 14:05:06 +0200

    Bump IOGX (#4)

--- flake.nix
+++ flake.nix
@@ -3,7 +3,30 @@
 
 
   inputs = {
-    iogx.url = "github:input-output-hk/iogx";
+    iogx = {
+      url = "github:input-output-hk/iogx";
+      inputs.hackage.follows = "hackage";
+      inputs.CHaP.follows = "CHaP";
+      inputs.haskell-nix.follows = "haskell-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixpkgs.follows = "haskell-nix/nixpkgs";
+
+    hackage = {
+      url = "github:input-output-hk/hackage.nix";
+      flake = false;
+    };
+
+    CHaP = {
+      url = "github:input-output-hk/cardano-haskell-packages?ref=repo";
+      flake = false;
+    };
+
+    haskell-nix = {
+      url = "github:input-output-hk/haskell.nix";
+      inputs.hackage.follows = "hackage";
+    };
   };
 
 
