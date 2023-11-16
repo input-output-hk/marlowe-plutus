2855f032e86afc4e0da9e7d5e1469bc9fac1acea
Brian W Bush <brian.bush@iohk.io>
2023-11-16 06:10:46 -0700

    PLT-8148 Migrate Charli3 oracle bridge.

--- marlowe-plutus/charli3/Main.hs
+++ marlowe-plutus/charli3/Main.hs
@@ -10,7 +10,7 @@ import Cardano.Binary (serialize')
 import qualified Data.ByteString as BS (writeFile)
 import qualified Data.ByteString.Base16 as B16 (encode)
 import qualified Language.Marlowe.Core.V1.Semantics.Types as V1
-import qualified Language.Marlowe.Scripts.Charli3 as Charli3 (validatorBytes, validatorHash)
+import qualified Language.Marlowe.Plutus.Charli3 as Charli3 (validatorBytes, validatorHash)
 import qualified Options.Applicative as O
 
 -- | The command-line arguments.
